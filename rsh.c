e5bc6bcaeb7f948ba241de226070ebb335beb806
Mohammed <nakhleh8877@gmail.com>
2026-07-10 19:46:05 +0300

    rush00 two version

---
+++ rsh.c
@@ -0,0 +1,35 @@
+#include <unitd.h>
+
+
+int main ()
+{
+	int y;
+	int column = 1;
+	int row = 1;
+
+	while (column= 1)
+	{
+		if ((row = 1) || (row = y))
+			write (1,"A",1);
+		else write (1,"B",1)
+		row++;
+	}
+	while (column = x)
+	{
+		row = 1;
+		if (row = 1 || row = y)
+			write (1,"C",1)
+		else write(1,"b",1)
+		row++
+	}
+
+	while (column != 1 && column != x)
+	{
+		row = 1;
+		if (row > 1 || row < y)
+			write (1," ",1);
+		else write (1,"B",1)
+		row++;
+	}
+	column++;
+}
---
+++ rush.c
@@ -0,0 +1,30 @@
+#include <unistd.h>
+int main(void)
+{
+	int y;
+	int x;
+	y = 5;
+	x = 5;
+	int r;  // raw
+	int c;  // column
+	c = 1;
+
+	while (c <= x)
+	{
+		r  = 1;
+		while (r <= y)
+		{
+			if (r == 1 && (c == 1 || c == y))
+				write(1,"A",1);
+			else if (r == x && (c == 1 || c == y))
+				write(1,"C",1);
+			else if (r == 1 ||  c == 1 || r == x || c == y)
+				write (1,"B",1);
+			else
+				write (1," ",1);
+			r++;
+		}
+		c++;
+		write(1,"\n",1);
+	}
+}
